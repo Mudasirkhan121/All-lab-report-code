df233ed7fdc6afda7b14d1f6a12001ca0e9f1ead
Mudasirkhan121 <mudasirkhanbnu07@gmail.com>
2021-08-10 23:05:52 +0500

    lab 2

---
+++ LAB NO.2/LAB2 Area of Rectangle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+
+  	 
+	printf("Programmer Name:MUDASIR KHAN\nReg.No:20MDELE121\n\n");
+	
+	int L,B,Area;
+	printf("plz enter Length and Bredth of a Rectangle:");
+	scanf("%d%d",&L,&B);
+	Area=L*B;
+	printf("The Required Area of Rectangle is:%d",Area);
+	
+	
+}
+
+
---
+++ LAB NO.2/LAB2 Sum of Two numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	
+  	 
+	printf("Programmer Name:MUDASIR KHAN\nReg.No:20MDELE121\n\n");
+	
+	int num1,num2,sum;
+	printf("plz enter two numbers:");
+	scanf("%d%d",&num1,&num2);
+	sum=num1+num2;
+	printf("The Sum of %d and %d is:%d",num1,num2,sum);
+	
+	
+}
+
+
