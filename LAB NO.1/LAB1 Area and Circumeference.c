d8990463fc0ca485068b6b1e74fdd3fd09ad15e8
Mudasirkhan121 <mudasirkhanbnu07@gmail.com>
2021-08-10 23:03:26 +0500

    Create LAB1 Area and Circumeference.c

---
+++ LAB NO.1/LAB1 Area and Circumeference.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+	/*Programmer Name:MUDASIR KHAN
+	  Reg.No:20MDELE121           */
+  	 
+	printf("Programmer Name:MUDASIR KHAN\nReg.No:20MDELE121\n\n");
+	
+	int r;
+	float pi=3.14,Area,Circumeference;
+	printf("plz inter a radius:");
+	scanf("%d",&r);
+	Area = pi*r*r;
+	Circumeference = 2*pi*r;
+	printf("The Required Area of a Circle is:%g",Area);
+	printf("\nThe Required Circumeference of a Circle is:%g",Circumeference);
+	
+	
+}
+
+
